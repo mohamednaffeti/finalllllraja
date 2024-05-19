7eb1aaea531b946a14035fd2f0046e3db6e8157b
mnaffeti <mohamednaffati08@gmail.com>
2024-05-19 17:20:13 +0100

    first commit

---
+++ src/main/java/com/cpg/mutuelle/repositories/ImageRepository.java
@@ -0,0 +1,17 @@
+package com.cpg.mutuelle.repositories;
+
+import com.cpg.mutuelle.entities.Actualite;
+import com.cpg.mutuelle.entities.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image,Long> {
+    Optional<Image> findByActualite(Actualite actualite);
+
+
+    List<Image> findImagesByActualiteId(Long id);
+}
